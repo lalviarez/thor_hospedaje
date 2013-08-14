346cf7946b41b2b1be837344a854911d310e91b5
lalviarez <alviarez.leonardo@gmail.com>
2013-08-14 17:31:10 -0430

    Continuando el trabajo en el frontend de países

--- site/views/country/tmpl/default.php
+++ site/views/country/tmpl/default.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version		$Id: default.php 2013-07-11
+ * @version		$Id: default.php 2013-08-14
  * @copyright	Copyright (C) 2013 Leonardo Alviarez - Edén Arreaza. All Rights Reserved.
  * @license		GNU General Public License version 3, or later
  * @note		Note : All ini files need to be saved as UTF-8 - No BOM
@@ -13,16 +13,16 @@ JHtml::addIncludePath(JPATH_COMPONENT.'/helpers');
 JHtml::_('behavior.framework');
 JHtml::_('bootstrap.framework');
 $document = JFactory::getDocument();
-$document->addStyleSheet('media/com_thor_hospedaje/css/th_countries.css');
+$document->addStyleSheet('media/com_thor_hospedaje/css/th_states.css');
 $document->addScript('media/com_thor_hospedaje/js/jquery.ThreeDots.min.js');
 $document->addScriptDeclaration('
 jQuery(document).ready(function(){
     jQuery(window).on("resize", function () {
 
-    var w = jQuery("[id^=th-country-image]").outerWidth(true);
-    var h = jQuery("[id^=th-country-image]").outerHeight(true);
+    var w = jQuery("[id^=th-state-image]").outerWidth(true);
+    var h = jQuery("[id^=th-state-image]").outerHeight(true);
     
-    jQuery("[id^=th-country-text]").height(h);
+    jQuery("[id^=th-state-text]").height(h);
 
 	}).resize();
     
@@ -35,30 +35,34 @@ $rowCount = 2;
 $itemRow = 2;
 $itemWidth = 47;
 ?>
+
 <?php /* Falta agregar y probar el uso de la clase que el usuario pasa por parametro */ ?>
-<div class="mod_th_countries">
+<div class="mod_th_states">
+	<div class="row-fluid">
+		<p><?php echo nl2br($this->item->country_desc); ?></p>
+	</div>
 	<?php
 	$count = 0;
-	for ($i = $count;  $i < count($this->items); $i++):
-		if (isset($this->items[$count])): 
+	$list = $this->item->states;
+	for ($i = $count;  $i < count($list); $i++):
+		if (isset($list[$count])): 
 	?>		<div class="row-fluid">
 			<?php
 			for ($j = 0; $j < $itemRow; $j++):
-				if (isset($this->items[$count])):
-					$item = $this->items[$count];
+				if (isset($list[$count])):
+					$item = $list[$count];
 			?>
 
-			<a href="<?php echo JRoute::_('index.php?view=country&id='. $item->id);?>">
-			<div class="country-item" style="width: <?php echo $itemWidth; ?>%;">
-				<div id="th-country-image-<?php echo $count;?>" class="country-image">
+			<a href="<?php echo JRoute::_('index.php?view=state&id='. $item->id);?>">
+			<div class="state-item" style="width: <?php echo $itemWidth; ?>%;">
+				<div id="th-state-image-<?php echo $count;?>" class="state-image">
 					<img src="<?php echo $item->image?>">
 				</div>
-				<div id="th-country-text-<?php echo $count;?>" class="country-text">
-					<h1><?php echo $item->country; ?></h1>
-					<div class="ellipsis"><span class="ellipsis_text"><?php echo $item->country_desc; ?></span></div>
+				<div id="th-state-text-<?php echo $count;?>" class="state-text">
+					<h1><?php echo $item->state_name; ?></h1>
+					<div class="ellipsis"><span class="ellipsis_text"><?php echo $item->state_desc; ?></span></div>
 				</div>
-			</div>
-			</a>
+			</div></a>
 			<?php
 					$count++;
 				endif;
---
+++ site/views/country/view.html.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * @version		$Id: view.html.php 2013-08-14
+ * @copyright	Copyright (C) 2013 Leonardo Alviarez - Edén Arreaza. All Rights Reserved.
+ * @license		GNU General Public License version 3, or later
+ * @note		Note : All ini files need to be saved as UTF-8 - No BOM
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// Import model states
+require_once JPATH_COMPONENT.'/models/states.php';
+
+// import Joomla view library
+jimport('joomla.application.component.view');
+
+/**
+ * Countries View
+ */
+class ThorHospedajeViewCountry extends JViewLegacy
+{
+	
+	protected $pagination;
+	
+	protected $state;
+
+	protected $item;
+
+	/**
+	 * Country view display method
+	 * @return void
+	 */
+	function display($tpl = null) 
+	{
+		$app		= JFactory::getApplication();
+		
+		// Get data from the model
+		$this->item		= $this->get('Item');
+		$this->state	= $this->get('State');
+		//$this->pagination	= $this->get('Pagination');  
+
+		if ($this->item)
+		{
+			// Get States Model data
+			$statesModel = JModelLegacy::getInstance('States', 'ThorHospedajeModel', array('ignore_request' => true));
+			$statesModel->setState('list.select', 'a.id, a.state_name, a.state_desc, a.image, a.ordering');
+		
+			// Filter by state of publicated
+			$statesModel->setState('filter.state', 1);
+
+			// Filter by language
+			$statesModel->setState('filter.language', $app->getLanguageFilter());
+			
+			// Filter by Country
+			$id_country	= $this->item->id;
+			$statesModel->setState('filter.id_country', $id_country);
+			
+			// Ordering
+			$statesModel->setState('list.ordering', 'ordering');
+			$statesModel->setState('list.direction','asc');
+			
+			// Set the limits for pagination
+			$limitstart = $app->input->get('limitstart', 0, 'uint');
+			$statesModel->setState('list.start', $limitstart);
+			// LJAH: Esto debe cambiarse por parametros pasados por usuario
+			$countItems = 4;
+			$statesModel->setState('list.limit', $countItems);
+
+			$this->item->states = $statesModel->getItems();
+			$this->pagination = $statesModel->getPagination();
+		}
+		
+        // Check for errors.
+		if (count($errors = $this->get('Errors'))) 
+		{
+			JError::raiseError(500, implode('<br />', $errors));
+			return false;
+		}
+		
+		// Display the template
+		parent::display($tpl);
+	}
+}
