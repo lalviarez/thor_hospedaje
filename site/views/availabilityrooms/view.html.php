870b587183f3fe7625b366d8beff22ec66bab22e
lalviarez <alviarez.leonardo@gmail.com>
2013-08-27 17:00:33 -0430

    Trabajando con la busqueda de habitaciones

---
+++ site/views/availabilityrooms/view.html.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @version		$Id: view.html.php 2013-07-11
+ * @copyright	Copyright (C) 2013 Leonardo Alviarez - Edén Arreaza. All Rights Reserved.
+ * @license		GNU General Public License version 3, or later
+ * @note		Note : All ini files need to be saved as UTF-8 - No BOM
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// import Joomla view library
+jimport('joomla.application.component.view');
+
+/**
+ * Availability rooms View
+ */
+class ThorHospedajeViewAvailabilityRooms extends JViewLegacy
+{
+	protected $state;
+	
+	protected $items;
+
+	protected $pagination;
+	
+	protected $params;
+
+	/**
+	 * Availability rooms view display method
+	 * @return void
+	 */
+	function display($tpl = null) 
+	{
+		// Get data from the model
+		//$this->state		= $this->get('State');
+		$this->items		= $this->get('Items');
+		//$this->pagination	= $this->get('Pagination'); 
+		//$this->params = &$this->state->params; 
+
+        // Check for errors.
+		if (count($errors = $this->get('Errors'))) 
+		{
+			JError::raiseError(500, implode('<br />', $errors));
+			return false;
+		}
+		
+		// Display the template
+		parent::display($tpl);
+	}
+}
